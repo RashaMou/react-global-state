d0f033ff5d4752ec022ee919a748ef064ff1eb5a
Rasha Moumneh <rasha@fastmailteam.com>
2021-08-24 17:10:48 -0400

    Implement skeleton of app reducer

---
+++ src/AppContext.js
@@ -0,0 +1,16 @@
+import { createContext, useContext, useReducer } from "react";
+import { BOOKS } from "./constants";
+
+const appStateReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  toRead: BOOKS,
+  completed: [],
+};
+
+export function AppStateProvider({ children }) {}
